26025715569bd61c2b78e8f4ee0187e8b1afc19b
obembeg <42247705+obembeg@users.noreply.github.com>
2026-09-12 20:06:01 +0100

    updated CORS in app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL].filter(Boolean),
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     credentials: true,
   }),
